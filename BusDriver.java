6a93837d3aa7b92b517fb7916ca7da8a66a789af
aymanbb <aymancool30@gmail.com>
2025-09-19 11:24:27 +0200

    Bus driver
    Added a bus driver class

---
+++ BusDriver.java
@@ -0,0 +1,11 @@
+public class BusDriver {
+    public String name;
+    public int age;
+    public int routeID;
+
+    public BusDriver(String name, int age, int routeID) {
+        this.name = name;
+        this.age = age;
+        this.routeID = routeID;
+    }
+}
