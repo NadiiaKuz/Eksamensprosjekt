fd61dc4e4b45c1efbce203f9425b7a456e6f7e2e
Ayman Bellamine <53999743+aymanbb@users.noreply.github.com>
2025-09-19 11:30:02 +0200

    Update BusDriver.java

--- EksamensprosjektSET/Practice/BusDriver.java
+++ EksamensprosjektSET/Practice/BusDriver.java
@@ -2,10 +2,12 @@ public class BusDriver {
     public String name;
     public int age;
     public int routeID;
+    public float meanness_scale;
 
-    public BusDriver(String name, int age, int routeID) {
+    public BusDriver(String name, int age, int routeID, float meanness_scale) {
         this.name = name;
         this.age = age;
         this.routeID = routeID;
+        this.meanness_scale = meanness_scale;
     }
 }
