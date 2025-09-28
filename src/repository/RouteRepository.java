2bb02be37cd34a243d626841373677ed7411b2bd
NadiiaKuz <NadinK1603@gmail.com>
2025-09-28 13:43:08 +0200

    added necessary classes for reiserute forslag

---
+++ src/repository/RouteRepository.java
@@ -0,0 +1,27 @@
+package repository;
+
+import model.Route;
+
+import java.util.ArrayList;
+
+public class RouteRepository {
+
+    public Route getRouteById(int routeId){
+
+        return null;
+    }
+
+    public ArrayList<Route> getAllRouts(){
+
+        ArrayList<Route> listOfRouts = new ArrayList<>();
+
+        return listOfRouts;
+    }
+
+    public ArrayList<Route> getRouteByStopId(int stopID){
+
+        ArrayList<Route> listOfRouts = new ArrayList<>();
+
+        return listOfRouts;
+    }
+}
---
+++ src/repository/StopRepository.java
@@ -0,0 +1,25 @@
+package repository;
+
+import model.Stop;
+
+import java.util.ArrayList;
+
+public class StopRepository {
+
+    public Stop getStopById(int stopId){
+
+        return null;
+    }
+
+    public Stop getStopByName(String name){
+
+        return null;
+    }
+
+    public ArrayList<Stop> getAllStops(){
+
+        ArrayList<Stop> listOfStops = new ArrayList<>();
+
+        return listOfStops;
+    }
+}
